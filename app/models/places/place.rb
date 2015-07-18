4eca2dee72473e63b17dc558a5cdf6857d40b170
hgillesp <hsgvmd@gmail.com>
2015-07-17 23:01:08 -0700

    need to figure out how to hide the interface to children/parents

--- app/models/places/place.rb
+++ app/models/places/place.rb
@@ -2,24 +2,6 @@ module Places
   class Place
     include ::Mongoid::Document
 
-    # this uses a Factory pattern to insanciate the Places; this, 
-    # hopefully, will allow enforcement of the appropriate hierarchy and 
-    # connections
-    # class << self
-    #     def create_place ()
-    #         p = new
-    #         @s_place_root ||= p
-    #         return p
-    #     end
-
-    #     def root
-    #         return @s_place_root
-    #     end
-          
-    #     private :new
-    # end
-    # public
-    # data fields for this class
     field :name, type: String         # name of place
     field :description, type: String  # description
 
@@ -57,26 +39,15 @@ module Places
             self.children.delete(ch)
         end
     end
-    
+
+    # don't want to index off of place name? - could have multiple entries w. similar names
     # create an index off of the place name, alone; will later create on off of the
     # geo locations *additionally*
     # index({ place_name: 1 }, { unique: false, name: "name_index"})
 
-    # # to_param method for 
-    # def to_param
-    #     @place_name
-    # end
+    # ** would like to hide these interfaces in a private/protected manner, not clear 
+    # ** how to do this in Ruby - doesn't have 'const' access similar to c++, etc. 
 
-    # protected
-
-    #     # wrap up initialization after new called;
-    #     def initialize ()
-    #     end
-
-    # private
-    #     # class variable - root of a place; this will deprecate but is useful for testing
-    #     @@s_place_root = nil
-    # private
     # relationships to other places/objects; each place may have multiple child
     # places. If the parent place is deleted, then an error is raised as the children
     # would be orphaned - this can be handled later. If the child is deleted, the parent-child
@@ -86,6 +57,5 @@ module Places
     has_many :children, class_name: "Places::Place" # places within this place
     belongs_to :parent, class_name: "Places::Place", dependent: :nullify  # places containing this place 
           
-
   end   # class Place
 end # module Places
